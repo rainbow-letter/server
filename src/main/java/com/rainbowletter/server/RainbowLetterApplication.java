36e63bb3cac7846ab6c55e2e05e5518b6d466fe2
Handwoong <handwoong@gmail.com>
2024-06-09 15:11:25 +0900

    :tada: initialize project

---
+++ src/main/java/com/rainbowletter/server/RainbowLetterApplication.java
@@ -0,0 +1,13 @@
+package com.rainbowletter.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainbowLetterApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(RainbowLetterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rainbowletter/server/RainbowLetterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rainbowletter.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RainbowLetterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
