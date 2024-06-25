d54e0166de20c324988ad3d52b7da2513b7789e5
Handwoong <handwoong@gmail.com>
2024-06-25 22:25:05 +0900

    docs: add letter api document

--- src/main/java/com/rainbowletter/server/common/config/security/uri/AccessAllowUri.java
+++ src/main/java/com/rainbowletter/server/common/config/security/uri/AccessAllowUri.java
@@ -6,8 +6,12 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum AccessAllowUri implements AllowUri {
+	ROOT("/"),
+	INDEX("/index.html"),
 	GET_FAQS("/api/faqs"),
 	GET_IMAGES("/api/images/resources/**"),
+	SHARE_LETTER("/api/letters/share/**"),
+	SHARE_REPLY("/api/replies/share/**"),
 	;
 
 	private final String uri;
